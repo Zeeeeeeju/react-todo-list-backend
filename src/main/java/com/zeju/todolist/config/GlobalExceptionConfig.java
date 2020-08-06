e568bccd1ba7c77ebe1aed9633f2cd5e39acea24
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-08-06 12:00:32 +0800

    [Quentin] feature: add global exception handler

---
+++ src/main/java/com/zeju/todolist/config/GlobalExceptionConfig.java
@@ -0,0 +1,31 @@
+package com.zeju.todolist.config;
+
+import com.zeju.todolist.exception.IllegalOperationException;
+import com.zeju.todolist.exception.NoSuchDataException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionConfig {
+
+    public static final String CAN_NOT_FIND_DATA = "Can not find data";
+    public static final String ILLEGAL_OPERATION = "Illegal operation!";
+
+    @ExceptionHandler(NoSuchDataException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String noSuchDataExceptionHandler() {
+        return CAN_NOT_FIND_DATA;
+    }
+
+    @ExceptionHandler(IllegalOperationException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String illegalOperationExceptionHandler() {
+        return ILLEGAL_OPERATION;
+    }
+
+}
