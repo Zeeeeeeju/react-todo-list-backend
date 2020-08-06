3dc35339260072868c2e14404c9a8ba27c558f3b
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-08-06 22:36:17 +0800

    [Quentin] test: add should_return_todoResponse_when_add_todo_given_todoRequest

--- src/test/java/com/zeju/todolist/integration/TodoIntegrationTest.java
+++ src/test/java/com/zeju/todolist/integration/TodoIntegrationTest.java
@@ -1,11 +1,19 @@
 package com.zeju.todolist.integration;
 
+import com.alibaba.fastjson.JSONObject;
+import com.zeju.todolist.dto.TodoRequest;
 import com.zeju.todolist.repository.TodoRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -17,5 +25,23 @@ public class TodoIntegrationTest {
     @Autowired
     private TodoRepository todoRepository;
 
+    @AfterEach
+    void tearDown() {
+        todoRepository.deleteAll();
+    }
 
+    @Test
+    void should_return_todoResponse_when_add_todo_given_todoRequest() throws Exception {
+        //given
+        TodoRequest todoRequest = new TodoRequest("abc",false);
+
+        //when
+        String todoJson = JSONObject.toJSONString(todoRequest);
+
+        mockMvc.perform(post("/todos").contentType(MediaType.APPLICATION_JSON).content(todoJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value(todoRequest.getContent()))
+                .andExpect(jsonPath("$.status").value(todoRequest.getStatus()));
+    }
 }
